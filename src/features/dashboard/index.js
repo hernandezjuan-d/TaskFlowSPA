cfeedf40b74f612ad09b0cf7e33c9d5ee903b958
hernandezjuan-d <hernandezvianajuan@gmail.com>
2026-06-04 17:51:00 -0500

    feat: dashboard and logut functional

--- src/features/dashboard/index.js
+++ src/features/dashboard/index.js
@@ -1,3 +1,18 @@
-export function render() {
-  document.getElementById("app").innerHTML = `<p>Dashboard - próximamente</p>`;
+import { logout } from "../auth/authService.js";
+import { navigateTo } from "../../router/index.js";
+
+export async function render() {
+  const response = await fetch("/src/features/dashboard/views/dashboard.html");
+  const html = await response.text();
+  document.getElementById("app").innerHTML = html;
+
+  const session = JSON.parse(localStorage.getItem("session"));
+  if (session) {
+    document.getElementById("user-name").textContent = session.name;
+  }
+
+  document.getElementById("logout-btn").addEventListener("click", () => {
+    logout();
+    navigateTo("/login");
+  });
 }
